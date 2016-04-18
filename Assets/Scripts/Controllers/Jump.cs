7c1e6854fed9f0967ad682d717f429635369d8f9
= <=>
2016-04-18 01:20:30 -0300

    Testes com FBX

---
+++ Assets/Scripts/Controllers/Jump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+    Player Player;
+
+    Rigidbody PlayerRigd;
+
+    [Range(0,5)]
+    public int JumpHeight;
+
+    void Awake()
+    {
+        this.Player = this.GetComponent<Player>();
+        this.PlayerRigd = Player.GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( this.Player.IsGrounded && Input.GetKeyDown( "space" ) )
+        {
+            ExecuteJump();
+        }
+    }
+
+    public void ExecuteJump()
+    {
+        PlayerRigd.AddForce( Vector3.up * ( JumpHeight * 100 ), ForceMode.Acceleration );
+    }
+}
---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// Se o player está no chão
+    /// </summary>
+    public bool IsGrounded { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter( Collision collision )
+    {
+        this.IsGrounded = true;
+    }
+
+    void OnCollisionStay( Collision collision )
+    {
+        this.IsGrounded = true;
+    }
+
+    void OnCollisionExit( Collision collision )
+    {
+        this.IsGrounded = false;
+    }
+
+}
